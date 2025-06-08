c87ea7772c3daf1f349ac27635532732660243ce
yyogue <yousyogue@gmail.com>
2025-06-08 06:12:05 -0400

    api

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5002';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://evn92jcmry.us-east-1.awsapprunner.com';
 
 // Create axios instance
 const api = axios.create({
